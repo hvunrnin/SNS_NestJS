77ad0a336d357fdc9fad91a16e0b6b11b0261a45
hvunrnin <hmkwon0919@cau.ac.kr>
2024-03-05 17:08:10 +0900

    JSON 반환

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+
+interface Post{
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'aaa',
+      title: '111',
+      content: 'qwerty',
+      likeCount: 1000000,
+      commentCount: 99999,
+    };
   }
 }
