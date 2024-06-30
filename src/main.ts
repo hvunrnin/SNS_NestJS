776f31740444db175cd96a54434cfa67a18dbc7b
hvunrnin <hmkwon0919@cau.ac.kr>
2024-06-30 14:01:54 +0900

    swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); //이 AppModule로부터 모듈을 확장해나가고 이 안에 있는 provider 관리하면 됨
@@ -8,6 +9,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe()); 
   // create-post.dto 처럼 만들어낸 모든 클래스 validator들의 annotation(isString 등) 따로 추가 안 하고 앱 전반적으로 전부 적용 가능
 
+  const config = new DocumentBuilder()
+  .setTitle('SNS')
+  .setDescription('SNS API description')
+  .setVersion('1.0')
+  .addTag('swagger')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
